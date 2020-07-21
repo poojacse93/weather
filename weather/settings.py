5ce402c8c3f79e4f1207e66905199785b558798c
poojacse93 <rvps19@gmail.com>
2020-07-21 16:40:28 +0530

    message

--- weather/settings.py
+++ weather/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fy@kll4u@-@psb4hf!r22ylwuyn8r*-4d_gb5qle8q#2i=%-j*'
+SECRET_KEY = os.environ.get('fy@kll4u@-@psb4hf!r22ylwuyn8r*-4d_gb5qle8q#2i=%-j*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
