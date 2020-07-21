1988d5cd4e4f70f7a5a9a5717fc0111b41d6e96c
poojacse93 <rvps19@gmail.com>
2020-07-21 17:08:28 +0530

    msg

--- weather/settings.py
+++ weather/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fy@kll4u@-@psb4hf!r22ylwuyn8r*-4d_gb5qle8q#2i=%-j*'
+#SECRET_KEY = 'fy@kll4u@-@psb4hf!r22ylwuyn8r*-4d_gb5qle8q#2i=%-j*'
 SECRET_KEY = os.environ.get('fy@kll4u@-@psb4hf!r22ylwuyn8r*-4d_gb5qle8q#2i=%-j*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
